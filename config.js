9598daf0d1a6345b18e31f40673774b72a662008
hamiltonnypolice-svg <hamiltonnypolice@gmail.com>
2026-09-17 15:13:22 -0400

    Add shared database configuration

---
+++ config.js
@@ -0,0 +1,4 @@
+// Shared Google Sheets backend for Hamilton Appearance Tickets.
+// Paste the Google Apps Script Web App URL ending in /exec between the quotes below.
+// Example: https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXXXXXX/exec
+window.HAMILTON_BACKEND_URL = 'PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE';
